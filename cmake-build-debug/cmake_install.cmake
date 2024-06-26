5de1b0cf7706fde0e8d176d47c0797d059291387
UniteAman <amansid1427@gmail.com>
2024-06-26 11:06:16 +0530

    first commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Hp/CLionProjects/RubiksCubeSolver
+# Install script for directory: C:/Users/amans/CLionProjects/RubiksCubeSolver
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Hp/CLionProjects/RubiksCubeSolver/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/amans/CLionProjects/RubiksCubeSolver/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
