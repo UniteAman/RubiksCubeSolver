370b4b96cb0164d956314948344b39c8735b88d9
UniteAman <amansid1427@gmail.com>
2024-06-26 11:09:02 +0530

    first commit

---
+++ cmake-build-debug/CMakeFiles/RubiksCubeSolver.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RubiksCubeSolver.dir/main.cpp.obj"
+  "CMakeFiles/RubiksCubeSolver.dir/main.cpp.obj.d"
+  "RubiksCubeSolver.exe"
+  "RubiksCubeSolver.exe.manifest"
+  "RubiksCubeSolver.pdb"
+  "libRubiksCubeSolver.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RubiksCubeSolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
